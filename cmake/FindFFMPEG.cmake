53a6bdc7fc6febcf466ff2ee873110360a958a10
Alexey Sachkov <sachkov2011@gmail.com>
2018-07-15 14:01:04 +0300

    [CMake] Fixes to find FFMPEG library

--- cmake/FindFFMPEG.cmake
+++ cmake/FindFFMPEG.cmake
@@ -9,6 +9,8 @@
 FIND_PATH(AVUTIL_INCLUDE_DIR
 	NAMES
 		avutil.h
+	HINTS
+		ENV FFMPEG_DIR
 	PATHS
 		/usr/include
 		/usr/include/x86_64-linux-gnu
@@ -23,12 +25,17 @@ FIND_PATH(AVUTIL_INCLUDE_DIR
 		/usr/include/ffmpeg/libavutil
 	PATH_SUFFIXES
 		libavutil
+		include/libavutil
 		ffmpeg
 )
+
+#message("AVUTIL_INCLUDE_DIR is ${AVUTIL_INCLUDE_DIR}")
 
 FIND_PATH(AVCODEC_INCLUDE_DIR
 	NAMES
 		avcodec.h
+	HINTS
+		ENV FFMPEG_DIR
 	PATHS
 		/usr/include
 		/usr/include/x86_64-linux-gnu
@@ -43,12 +50,17 @@ FIND_PATH(AVCODEC_INCLUDE_DIR
 		/usr/include/ffmpeg/libavcodec
 	PATH_SUFFIXES
 		libavcodec
+		include/libavcodec
 		ffmpeg
 )
+
+#message("AVCODEC_INCLUDE_DIR is ${AVCODEC_INCLUDE_DIR}")
 
 FIND_PATH(AVFORMAT_INCLUDE_DIR
 	NAMES
 		avformat.h
+	HINTS
+		ENV FFMPEG_DIR
 	PATHS
 		/usr/include
 		/usr/include/x86_64-linux-gnu
@@ -63,14 +75,19 @@ FIND_PATH(AVFORMAT_INCLUDE_DIR
 		/usr/include/ffmpeg/libavformat
 	PATH_SUFFIXES
 		libavformat
+		include/libavformat
 		ffmpeg
 )
+
+#message("AVFORMAT_INCLUDE_DIR is ${AVFORMAT_INCLUDE_DIR}")
 
 FIND_LIBRARY(AVUTIL_LIBRARY
 	NAMES
 		avutil
 		avutil-55
 		avutil-56
+	HINTS
+		ENV FFMPEG_DIR
 	PATHS
 		/usr/lib
 		/usr/lib/x86_64-linux-gnu
@@ -83,13 +100,19 @@ FIND_LIBRARY(AVUTIL_LIBRARY
 		/local/bin
 		/mingw/bin
 		/bin
+	PATH_SUFFIXES
+		lib
 )
+
+#message("AVUTIL_LIBRARY is ${AVUTIL_LIBRARY}")
 
 FIND_LIBRARY(AVCODEC_LIBRARY
 	NAMES
 		avcodec
 		avcodec-57
 		avcodec-58
+	HINTS
+		ENV FFMPEG_DIR
 	PATHS
 		/usr/lib
 		/usr/lib/x86_64-linux-gnu
@@ -101,13 +124,19 @@ FIND_LIBRARY(AVCODEC_LIBRARY
 		/local/bin
 		/mingw/bin
 		/bin
+	PATH_SUFFIXES
+		lib
 )
+
+#message("AVCODEC_LIBRARY is ${AVCODEC_LIBRARY}")
 
 FIND_LIBRARY(AVFORMAT_LIBRARY
 	NAMES
 		avformat
 		avformat-57
 		avformat-58
+	HINTS
+		ENV FFMPEG_DIR
 	PATHS
 		/usr/lib
 		/usr/lib/x86_64-linux-gnu
@@ -120,7 +149,11 @@ FIND_LIBRARY(AVFORMAT_LIBRARY
 		/mingw/bin
 		/mingw/lib
 		/bin
+	PATH_SUFFIXES
+		lib
 )
+
+#message("AVFORMAT_LIBRARY is ${AVFORMAT_LIBRARY}")
 
 SET(FFMPEG_INCLUDE_DIRS
 	#${AVUTIL_INCLUDE_DIR}
@@ -150,6 +183,9 @@ IF(AVFORMAT_LIBRARY)
 		${AVFORMAT_LIBRARY}
 	)
 ENDIF(AVFORMAT_LIBRARY)
+
+#message("FFMPEG_INCLUDE_DIRS is ${FFMPEG_INCLUDE_DIRS}")
+#message("FFMPEG_LIBRARIES is ${FFMPEG_LIBRARIES}")
 
 IF(FFMPEG_INCLUDE_DIRS AND FFMPEG_LIBRARIES)
 	SET(FFMPEG_FOUND TRUE)
